e8126d5a3a65a3419426cba28fc53cd30ac4f765
Scott Frees <scott.frees@gmail.com>
2014-12-27 20:12:09 -0500

    put/post for adding files

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
 var express = require('express');
-var fx = express.Router();
+var Grid = require('gridfs-stream');
+var fs = require('fs');
 
-fx.get('/', function(req, res, next) {
-  res.json({ });
-});
 
-fx.get('/:owner/', function(req, res, next) {
-  res.json({ });
-});
+var fx = function (mongo, db) {
 
-exports.router = fx;
+  var router = express.Router();
+  var gfs = Grid(db, mongo);
+
+  var add = function(req, res, next) {
+    if ( !req.body.metadata ) {
+      res.status(406).send('metadata must be sent along with the new file');
+      return;
+    }
+
+    var metadata = JSON.parse(req.body.metadata)
+    if (!metadata.filename) {
+      res.status(406).send('filename is required property of metadata field');
+      return;
+    }
+
+    var writestream = gfs.createWriteStream({filename : metadata.filename});
+    fs.createReadStream(req.files.file.file).pipe(writestream);
+    writestream.on('close', function (file) {
+      res.json({ _id:file._id});
+    });
+  }
+
+  var list = function(req, res, next) {
+    res.json({ });
+  };
+
+  router.get('/', function(req, res, next) {
+    res.json({ });
+  });
+
+  router.get('/:owner/', list);
+  router.put('/:owner/', add);
+  router.post('/:owner/', add);
+
+  return router;
+}
+
+
+
+exports.make_router = fx;
--- test/test.js
+++ test/test.js
@@ -1,14 +1,32 @@
 var assert = require('assert');
+var should = require('should');
 var express = require('express');
+var fx = require('..');
 var request;
 
 
 
 describe('fx() fresh', function(){
-  var server;
-  before(function(){
-    server = createServer();
-    request = require('supertest')(server);
+  var app;
+  var db;
+
+  before(function(done){
+    app = createServer();
+    request = require('supertest')(app);
+
+    var mongo = require('mongodb');
+    db = new mongo.Db('fx_test', new mongo.Server("127.0.0.1", 27017), {safe:false});
+
+    db.open(function (err) {
+      assert.equal(null, err);
+      var router = fx.make_router(mongo, db);
+      app.use('/', router);
+      done();
+    })
+  });
+
+  after(function(){
+    db.dropDatabase();
   })
 
   it('should return {} at root request', function(done){
@@ -20,13 +38,40 @@ describe('fx() fresh', function(){
     request.get('/foobar/')
            .expect(200, '{}', done)
   })
+
+  it('should allow a file to be put', function(done) {
+    request.put('/foo/')
+           .field('metadata', '{"filename":"test.txt"}')
+           .attach('file', 'test/files/test.txt')
+           .expect(has_id)
+           .end(done)
+  });
+
+  it('should allow a file to be posted', function(done) {
+    request.post('/foo/')
+           .field('metadata', '{"filename":"test.txt"}')
+           .attach('file', 'test/files/test.txt')
+           .expect(has_id)
+           .end(done)
+  });
 });
 
-
+function has_id(res) {
+  if ( res.status != 200 ) {
+    return "Response was expected to be 200";
+  }
+  if (!('_id' in res.body)) return "Response was missing _id key";
+}
 
 function createServer() {
   var app = express();
-  var fx = require('..');
-  app.use('/', fx.router);
+  var bb = require('express-busboy');
+
+
+  bb.extend(app, {
+    upload: true,
+    path: 'test/files/uploads'
+  });
+
   return app;
 }
